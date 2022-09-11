00fefb90b1ee51e53927d24f9a0984bab1bd01e2
Mariana <m.laic@hotmail.com>
2022-09-11 18:33:37 +0200

    Lade till bilder

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('My first class in JS');
+
+function minFunktion(minForm) {
+    let children = document.getElementById("form").children  //dokumenten sparas i variabeln children 
+    
+    
+    for(let i=0; i<children.length; i++){
+            console.log(children.value);
+    }
+       
+}
+function minFunktion(minForm) {
+    let formFyllnad = document.form["minForm"]["förNamn"]["efterNamn"]["mailAdress"].value;
+    if (formFyllnad == "") {
+      alert("Vänligen fyll i namn, efternamn och mail adress");
+      
+    }
+    else{
+        document.getElementById("tack","&#128151").innerHTML="Tack!, vi hör av oss så fort vi kan."
+
+    }
+  }
+
+
+
+
+
